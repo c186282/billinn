d7282408e552465a4f434c3a7c280eafb5bf3a52
Brad Paul <c186282@gamil.com>
2025-07-10 17:13:35 -0400

    fitst code

---
+++ src/billinn/billinn.py
@@ -0,0 +1,16 @@
+import astropy.units as u
+
+class Vehicle:
+
+    def __init__(self,
+                 weight: u.Quantity[u.kg],
+                 length: u.Quantity[u.m],
+                 width : u.Quantity[u.m]
+                 ):
+        self.weight = weight
+        self.length = length
+        self.width = width
+
+    @property
+    def area(self)-> u.Quantity[u.m**2]:
+        return self.width * self.length
