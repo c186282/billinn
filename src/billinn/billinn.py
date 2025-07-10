919a749d899bc1aabce46f2b93bf749e0727b38d
Brad Paul <c186282@gamil.com>
2025-07-10 18:26:02 -0400

    jupyter notebook

--- src/billinn/billinn.py
+++ src/billinn/billinn.py
@@ -21,5 +21,12 @@ class Car(Vehicle):
     def __init__(self,
                  weight: u.Quantity[u.kg],
                  length: u.Quantity[u.m],
-                 width : u.Quantity[u.m]
+                 width : u.Quantity[u.m],
+                 engine_size: u.Quantity[u.liter] = 1.0 * u.liter
                  ):
+        super().__init__(weight, length, width)
+        self.engine_size = engine_size
+
+    @property
+    def density(self) -> u.Quantity[u.kg / u.m**2]:
+        return self.weight / self.area
