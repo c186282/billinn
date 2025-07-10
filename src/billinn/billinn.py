396004ec5a9b1af600e5c10777c28eb8e8b4ddfe
rgoessling1 <rgoesslng34@gmail.com>
2025-07-10 17:52:19 -0400

    ryans first edits

--- src/billinn/billinn.py
+++ src/billinn/billinn.py
@@ -14,3 +14,12 @@ class Vehicle:
     @property
     def area(self)-> u.Quantity[u.m**2]:
         return self.width * self.length
+
+
+class Car(Vehicle):
+
+    def __init__(self,
+                 weight: u.Quantity[u.kg],
+                 length: u.Quantity[u.m],
+                 width : u.Quantity[u.m]
+                 ):
